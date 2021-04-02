02c61004508b0dead6ccbf372208484d031e0a90
Matthew Riegg <matthew.d.riegg@gmail.com>
2021-04-02 02:42:29 -0700

    Deliverables

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
-// Your code here
+dodger.style.backgroundColor = "#1775"
+
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+    }
+  });
+
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+    
+    document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+
+
+
+
+
+
+  document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left2 = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left2 + 1}px`;
+    }
+  });
+
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left2 = parseInt(leftNumbers, 10);
+  
+    if (left2 < 360) {
+        dodger.style.left = `${left2 + 1}px`;
+    }
+  }
+    
+    document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+
+
+
+
+
